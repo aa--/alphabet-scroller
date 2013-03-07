e83a35c67c16a5ff89c54be6569ade2ca4c7cee8
aa-- <weihu254@gmail.com>
2013-03-07 18:30:45 +0800

    update scroller highlight

--- js/alphabet.js
+++ js/alphabet.js
@@ -4,29 +4,74 @@
 $(document).ready(function(){
     'use strict';
 
-    var $scroller       = $('#scroller'),
-        $list           = $('#list'),
-        $indexArray     = $list.find('[data-type]'),
-        $alphabetArray  = $scroller.find('a'),
-        listIndexArray  = [];
-    function updateHighlight(){
-        
+    var $scroller       = $('#scroller'),                   // 滚动条
+        $list           = $('#list'),                       // 列表
+        $indexArray     = $list.find('[data-type]'),        // 列表锚点的jquery对象
+        $alphabetArray  = $scroller.find('a'),              // 字母链接的jquery对象
+        $itemArray      = $list.find('li li'),
+        $firstItem      = $itemArray.eq(0),
+        listIndex       = '',                               // 列表索引组成的字符串
+        itemHeight      = $firstItem.height(),
+        listHeight      = $list.height(),
+        visiableNum     = parseInt(listHeight / itemHeight, 10),
+        n,
+        highlightStart  = '',
+        highlightEnd    = '',
+        $TopItem,
+        $BottomItem,
+        timer,
+        firstIndex      = '',                               // 索引首位
+        firstHref       = '',                               // 首位链接
+        $firstAlphabet;                                     // 首个字母链接jquery对象
+    //--- 更新滚动条 ----------------------------------------------
+    function updateHighlight() {
+        n = - parseInt($firstItem.position().top / itemHeight, 10);
+        highlightStart = $itemArray.eq(n)
+                                   .text();
+        highlightStart = unicodeOffsetA(highlightStart);
+        highlightEnd = $itemArray.eq(n+visiableNum)
+                                 .text();
+        highlightEnd = unicodeOffsetA(highlightEnd);
+        $alphabetArray.removeClass('highlight');
+        $alphabetArray.slice(highlightStart, highlightEnd + 1)
+                      .addClass('highlight');
     }
-    $indexArray.each(function(){
+    //--- turn to charCode offset to 'A' ----------------------------------------------
+    function unicodeOffsetA(n) {
+        return n.toUpperCase().charCodeAt(0) - 65;
+    }
+    //--- initialize listIndex ----------------------------------------------
+    $indexArray.each(function() {
         var type = $(this).data('type');
-        listIndexArray.push(type);
+        listIndex += type;
     });
-    $alphabetArray.each(function(i){
+    //--- without type A ----------------------------------------------
+    firstIndex = listIndex.slice(0,1);
+    if(firstIndex !== 'A') {
+        $firstAlphabet = $alphabetArray.eq(0);
+        firstHref = $firstAlphabet.attr('href');
+        firstHref = firstHref.slice(0, -1) + firstIndex;
+        $firstAlphabet.attr('href', firstHref)
+                      .addClass('disabled');
+    }
+    //--- scroller map to list ----------------------------------------------
+    $alphabetArray.each(function(i) {
         var $this   = $(this),
             link    = $this.text(),
-            charCode= link.charCodeAt(0),
             lastLink;
-        // TODO: without type A
-        if($.inArray(charCode, listIndexArray) === -1) {
+        if(listIndex.indexOf(link) === -1 && i) {
             lastLink = $alphabetArray.eq(--i).attr('href');
             $this.attr('href', lastLink)
                  .addClass('disabled');
         }
     });
 
+    updateHighlight();
+    $list.bind('scroll', function() {
+        if(timer) {
+            clearTimeout(timer);
+            timer = 0;
+        }
+        timer = setTimeout(updateHighlight, 50);
+    });
 });
